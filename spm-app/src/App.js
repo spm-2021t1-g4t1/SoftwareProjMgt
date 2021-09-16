9540143b9a8e61595f6a5b8e24702ad98390e811
Shun Hui <56659883+Shun-97@users.noreply.github.com>
2021-09-16 22:38:37 +0800

    Added bunch of stuff
    Added:
    - Sidebar
    - Course Page
    - logo
    
    PlaceHolder
    - CourseList
    - Home Page

--- spm-app/src/App.js
+++ spm-app/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import MainSidebar from './component/MainSidebar.jsx';
+import Home from './pages/Home.jsx';
+import Course from './pages/Course.jsx';
+import CourseInfo from './pages/CourseInfo.jsx';
+
+import { BrowserRouter ,Route } from "react-router-dom"
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <MainSidebar />
+        <Route path = '/' component = {Home} exact/>
+        <Route path = '/course' component = {Course} exact/>
+        <Route path = '/course/:courseid' component = {CourseInfo} exact/>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
