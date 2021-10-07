797f54c0354019673385db3dd440d6ce8392710b
darrenho1994 <darkdrium_1994@hotmail.com>
2021-10-08 00:51:42 +0800

    fixed sectioncontainer.jsx retrieval of lesson materials

--- spm-app/src/component/SectionContainer.jsx
+++ spm-app/src/component/SectionContainer.jsx
@@ -2,37 +2,38 @@ import React, { useState } from 'react'
 
 const SectionContainer = (props) => {
     console.log(props.data)
-    
+
     let sectionArr = props.data
     // eslint-disable-next-line
-    const[sectionArrs, setSectionArrs] = useState(props.data)
+    const [sectionArrs, setSectionArrs] = useState(props.data)
 
     const sectionNumber = props.number
 
     // console.log(sectionArrs)
 
-    
+
     return (
-        <div className = 'sectionCon'>
-            <div className =' sectionCon-Top'>
-                <h1>{ sectionNumber } {sectionArrs['lesson_name']}</h1>
+        <div className='sectionCon'>
+            <div className=' sectionCon-Top'>
+                <h1>{sectionNumber} {sectionArrs['lesson_name']}</h1>
             </div>
-            <div className = 'sectionCon-Content'>
-                <div className = 'sectionCon-Description'>
+            <div className='sectionCon-Content'>
+                <div className='sectionCon-Description'>
                     <h2>Description</h2>
                     <p>
                         {sectionArrs['lesson_description']}
                     </p>
                 </div>
-                <div className = 'sectionCon-Material'>
+                <div className='sectionCon-Material'>
                     <h2>Course Material</h2>
                     <ul>
                         {sectionArrs['lesson_materials'].map((mat, index) =>
-                            <li key={index}>{mat}</li>                        
-                        )}
+                            <li key={mat['lesson_no'] - mat['course_material_title']}>
+                                <a href={mat['link']}>{mat['course_material_title']}</a>
+                            </li>)}
                     </ul>
 
-                    <div className = 'sectionCon-Quiz'>
+                    <div className='sectionCon-Quiz'>
                         <button>Take Quiz</button>
                     </div>
                 </div>
