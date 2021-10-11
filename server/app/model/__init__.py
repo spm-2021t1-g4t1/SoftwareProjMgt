2e88c3d19ddfa07474eddfd94c1a21eee852345f
ycyc <60686773+izhcong1997@users.noreply.github.com>
2021-10-11 19:34:35 +0800

    minor modifications
    just commenting

--- server/app/model/__init__.py
+++ server/app/model/__init__.py
@@ -9,6 +9,8 @@ from model.lesson import *
 from model.materials_viewed import *
 from model.quiz_attempts import *
 from model.quiz import *
+from model.question import *
+from model.quizoptions import *
 from model.staff import *
 
 
