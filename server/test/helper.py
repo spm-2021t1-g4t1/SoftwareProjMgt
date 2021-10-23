c0fadae4e55c0fafd88af8f47de7eb2c9aa849dd
trishachua2019 <trisha.chua.2019@sis.smu.edu.sg>
2021-10-23 23:49:16 +0800

    tdd

--- server/test/helper.py
+++ server/test/helper.py
@@ -2,9 +2,9 @@ import os
 import sys
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)+ '/app'
+# parentdir = os.path.dirname(currentdir)+ '/app'
 # parentdir1 = os.path.dirname(parentdir) + '/app'
-# parentdir = os.path.dirname(currentdir) + '\\app'
+parentdir = os.path.dirname(currentdir) + '\\app'
 sys.path.append(parentdir)
 # print(sys.path)
 
