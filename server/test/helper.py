70f4022d8e1fce0503f2bf884c1ae2c38ead7c0d
trishachua2019 <trisha.chua.2019@sis.smu.edu.sg>
2021-10-24 00:28:19 +0800

    push

--- server/test/helper.py
+++ server/test/helper.py
@@ -2,9 +2,10 @@ import os
 import sys
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
-# parentdir = os.path.dirname(currentdir)+ '/app'
-# parentdir1 = os.path.dirname(parentdir) + '/app'
-parentdir = os.path.dirname(currentdir) + '\\app'
+if platform.system() == "Windows":
+    parentdir = os.path.dirname(currentdir) + '\\app'
+else:
+    parentdir = os.path.dirname(currentdir) + '/app'
 sys.path.append(parentdir)
 # print(sys.path)
 
